46c8ca3dc55ce02e3aa868864e1dd5570e8b1d7e
yangqinjun <qinjun.yang@shopcider.com>
2023-05-22 16:31:40 +0800

    ⚡️：前端重写

--- netty-simple-im/chat-client/src/router/index.js
+++ netty-simple-im/chat-client/src/router/index.js
@@ -1,19 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/Login.vue'
+import MainView from '../views/Main.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: LoginView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/main',
+    name: 'main',
+    component: MainView
   }
 ]
 
